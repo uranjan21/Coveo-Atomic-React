8e7529b8f8436fce82625e81b806cec152a1b8b9
Utsav Ranjan <104273473+uranjan21@users.noreply.github.com>
2024-03-06 00:27:28 +0530

    basic react page

--- atomic-react-project/src/App.tsx
+++ atomic-react-project/src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FunctionComponent, useState} from 'react';
+import {createRoot} from 'react-dom/client';
+import {HeaderLink} from './components/HeaderLink';
+import {FoldedResultListPage} from './pages/FoldedResultListPage';
+import {InstantResultsPage} from './pages/InstantResultsPage';
+import {RecsPage} from './pages/RecsPage';
+import {ResultListPage} from './pages/ResultListPage';
+import {TableResultListPage} from './pages/TableResultListPage';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const LIST_PAGE = 'Result list';
+const FOLDED_LIST_PAGE = 'Folded result list';
+const INSTANT_RESULTS_PAGE = 'Instant results';
+const TABLE_RESULT_LIST_PAGE = 'Table result list';
+const RECS_PAGE = 'Recs Interface';
+
+const pages = [
+  LIST_PAGE,
+  FOLDED_LIST_PAGE,
+  INSTANT_RESULTS_PAGE,
+  TABLE_RESULT_LIST_PAGE,
+  RECS_PAGE,
+];
+const App: FunctionComponent = () => {
+  const initialPage = pages.find((page) =>
+    decodeURIComponent(window.location.search).includes(`page=${page}`)
   );
-}
+  const [page, setPage] = useState(initialPage || LIST_PAGE);
+  return (
+    <>
+      <header>
+        <span className="pageTitle">{page} example</span>
+        <ul>
+          <HeaderLink page={LIST_PAGE} currentPage={page} setPage={setPage} />
+          <HeaderLink
+            page={FOLDED_LIST_PAGE}
+            currentPage={page}
+            setPage={setPage}
+          />
+          <HeaderLink
+            page={INSTANT_RESULTS_PAGE}
+            currentPage={page}
+            setPage={setPage}
+          />
+          <HeaderLink
+            page={TABLE_RESULT_LIST_PAGE}
+            currentPage={page}
+            setPage={setPage}
+          />
+          <HeaderLink page={RECS_PAGE} currentPage={page} setPage={setPage} />
+        </ul>
+      </header>
+      {page === LIST_PAGE && <ResultListPage />}
+      {page === FOLDED_LIST_PAGE && <FoldedResultListPage />}
+      {page === INSTANT_RESULTS_PAGE && <InstantResultsPage />}
+      {page === TABLE_RESULT_LIST_PAGE && <TableResultListPage />}
+      {page === RECS_PAGE && <RecsPage />}
+    </>
+  );
+};
 
 export default App;
